de4aae958064352b45f33a1d43ca7f49e961e20e
unknown <AphidRS>
2022-07-11 17:35:31 +0200

    Cambio proyecto

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 
-function left_color() {
-document.getElementsByClassName("left_box").style.background = "red";
+function select() {
+    document.body.addEventListener("click", (e)=>{ 
+    let self = e.target;
+    old_bg = self.style.background;
+    /*console.log(self);*/
+    self.style.background = self.style.background=='brown'? 'blue':'brown';
+    })
 
+
+    /*
+    setTimeout(function(){
+        self.style.background = old_bg;
+    }, 1000);
+    /*
+    setTimeout(function(){
+        example.style.background = old_bg;
+    }, 1000);*/
 }
+
+
+
