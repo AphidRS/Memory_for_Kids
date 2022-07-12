37ae78894a58ca753101814ddf4693957e7a640a
unknown <AphidRS>
2022-07-12 16:35:56 +0200

    Clean code without warnings

--- js/index.js
+++ js/index.js
@@ -4,17 +4,14 @@ function select() {
     document.body.addEventListener("click", (e)=>{
     let self = e.target;
     let self_id = self.getAttribute("id");
-    const old_bg = self.style.background;
-    let valid = self_id.includes("item");
-    console.log(self);
-    console.log(valid);
-    console.log(old_bg);
-
-    if (valid) {
-        self.style.background = 'yellow';
-        setTimeout(restore_bg, 1000);
+    if (self_id != null) {
+        let valid = self_id.includes("item");
+        if (valid) {
+            self.style.background = 'yellow';
+            setTimeout(restore_bg, 5000);
+        }
     }
-
+    
     function restore_bg(){
         self.style.background = 'green';
     
@@ -22,14 +19,3 @@ function select() {
     })
 }
 
-
-
-/*
-    let self = e.target;
-    let self_string = self.toString();
-    let old_bg = self.style.background;
-    let valid = self_string.includes('item');
-    console.log(self);
-    console.log(self_string);
-    console.log(valid);
-*/
