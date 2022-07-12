2c089a5b778f63e23c4735b7ca3bd3631e2ef2f3
unknown <AphidRS>
2022-07-12 11:56:37 +0200

    Return color

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
 
+
 function select() {
-    document.body.addEventListener("click", (e)=>{ 
-    let self = e.target,
-    old_bg = self.style.background;
-    self.style.background = self.style.background =='yellow';
+    document.body.addEventListener("click", (e)=>{
+    let self = e.target;
+    let self_id = self.getAttribute("id");
+    const old_bg = self.style.background;
+    let valid = self_id.includes("item");
+    console.log(self);
+    console.log(valid);
+    console.log(old_bg);
+
+    if (valid) {
+        self.style.background = 'yellow';
+        setTimeout(restore_bg, 1000);
+    }
+
+    function restore_bg(){
+        self.style.background = 'green';
+    
+    }
     })
-    /*
-    setTimeout(function(){
-        self.style.background = old_bg;
-    }, 1000);
- */
 }
 
 
 
+/*
+    let self = e.target;
+    let self_string = self.toString();
+    let old_bg = self.style.background;
+    let valid = self_string.includes('item');
+    console.log(self);
+    console.log(self_string);
+    console.log(valid);
+*/
