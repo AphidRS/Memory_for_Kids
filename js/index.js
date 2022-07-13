2b2f0360414c326e40ba900d81af18d2f0f21723
AphidRS <aphidrs@gmail.com>
2022-07-14 01:46:02 +0200

    Random generator

--- js/index.js
+++ js/index.js
@@ -1,21 +1,52 @@
+window.onload=init();
+let counter = 0;
+let selected = true;
 
+
+function init() {
+    for (let i = 1; i < 17; i++) {
+        if(document.getElementById(i) != null) {
+            document.getElementById(i).innerHTML = (Math.floor(Math.random() * 10 + 7));
+        }              
+    }
+}
 
 function select() {
     document.body.addEventListener("click", (e)=>{
-    let self = e.target;
-    let self_id = self.getAttribute("id");
-    if (self_id != null) {
-        let valid = self_id.includes("item");
-        if (valid) {
-            self.style.background = 'yellow';
-            setTimeout(restore_bg, 5000);
+        check();
+        if (selected=true) {
+            let self = e.target;
+            let self_id = self.getAttribute("id");
+            if (self_id != null) {
+                if (self_id > 0 && self_id < 17) {
+                    self.style.background = 'yellow';
+                    setTimeout(restore_bg, 5000);
+            }
         }
-    }
-    
-    function restore_bg(){
+
+    function restore_bg() {
+        /*change_value(self_id);*/
         self.style.background = 'green';
-    
+        let self_id = null;
+        counter--;
     }
-    })
+}else {
+
 }
+})
+}
+function check() {
+    counter++;
+    if (counter < 3) {
+        selected = true;
+    } else {
+        selected = false;
+    }
+}
+/*
+function change_value(id_object) {
+    console.log(id_object);
+    let item_object = document.getElementById(id_object).innerHTML = "2";
+
+}*/
 
